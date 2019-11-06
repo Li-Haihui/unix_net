f2822c01494dc0c820427d84cca665e7ea80e14d
LiHaiHui <l810901308@163.com>
2019-11-06 23:49:09 +0800

    更新project1_5

---
+++ project1_5/sourceClient.cpp
@@ -0,0 +1,68 @@
+//#include "unp.h"
+#include	<sys/types.h>	/* basic system data types */
+#include	<sys/socket.h>	/* basic socket definitions */
+
+#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
+#include	<arpa/inet.h>	/* inet(3) functions */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include<unistd.h>
+
+
+#define MAX_BUFFER (1024)
+
+int main(int argc, char **argv)
+{
+    int socketfd, dataLen;
+    char recvData[MAX_BUFFER + 1];
+
+    struct sockaddr_in servaddr;
+
+    if (argc != 2)
+    {
+        printf("usage argv error\n");
+        //err_quit("usage argv error");    
+    }
+
+    if((socketfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("create socket error\n");
+    }
+
+    bzero(&servaddr, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(13);
+
+    //if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+    {
+        printf("inet_pton %s error\n", argv[1]);
+    }
+
+    if(connect(socketfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
+    {
+        printf("connect error\n");
+    }
+
+    while((dataLen = read(socketfd, recvData, MAX_BUFFER)) > 0)
+    {
+        recvData[dataLen] = 0;
+        if(fputs(recvData, stdout) == EOF)
+        {
+            printf("fputs error\n");
+        }
+    }
+
+    if(dataLen < 0)
+    {
+        printf("read error\n");
+    }
+
+    return 0;
+
+}
+    
---
+++ project1_5/sourceServer.cpp
@@ -0,0 +1,49 @@
+#include	<sys/types.h>	/* basic system data types */
+#include	<sys/socket.h>	/* basic socket definitions */
+
+#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
+#include	<arpa/inet.h>	/* inet(3) functions */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+
+#define MAX_BUFFER (1024)
+#define LISTEN_NUM (10)
+
+int main(int argc, char **argv)
+{
+    int socketfd, connfd;
+    struct sockaddr_in servaddr;
+    char buff[MAX_BUFFER];
+    time_t ticks;
+
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(13);
+
+    bind(socketfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+
+    listen(socketfd, LISTEN_NUM);
+
+    for(; ;)
+    {
+        connfd = accept(socketfd, (struct sockaddr*)NULL, NULL);
+
+        ticks = time(NULL);
+
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+
+        write(connfd, buff, strlen(buff));
+
+        close(connfd);
+    }
+}
+
