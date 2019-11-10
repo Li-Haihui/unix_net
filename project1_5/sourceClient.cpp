4543d47b9c837815857a7380302b68d7288eaa56
LiHaiHui <l810901308@163.com>
2019-11-10 16:17:32 +0800

    修改端口使用问题

--- project1_5/sourceClient.cpp
+++ project1_5/sourceClient.cpp
@@ -1,15 +1,29 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <string.h>  
+#include <errno.h>  
+#include <sys/types.h>  
+#include <sys/socket.h>  
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <time.h>
+#include <unistd.h>
+
+
+
 //#include "unp.h"
-#include	<sys/types.h>	/* basic system data types */
-#include	<sys/socket.h>	/* basic socket definitions */
+//#include	<sys/types.h>	/* basic system data types */
+//#include	<sys/socket.h>	/* basic socket definitions */
 
-#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
-#include	<arpa/inet.h>	/* inet(3) functions */
+//#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
+//#include	<arpa/inet.h>	/* inet(3) functions */
 
-#include <stdio.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include<unistd.h>
+//#include <stdio.h>
+//#include <string.h>
+//#include <sys/stat.h>
+//#include <fcntl.h>
+//#include<unistd.h>
 
 
 #define MAX_BUFFER (1024)
@@ -29,23 +43,25 @@ int main(int argc, char **argv)
 
     if((socketfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("create socket error\n");
+        printf("create socket error: %s(errno: %d)\n",strerror(errno),errno);
+        exit(0);
     }
 
     bzero(&servaddr, sizeof(servaddr));
 
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(13);
+    //servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(8000);
 
-    //if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+    if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
     {
         printf("inet_pton %s error\n", argv[1]);
     }
 
     if(connect(socketfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
     {
-        printf("connect error\n");
+        printf("connect error: %s(errno: %d)\n",strerror(errno),errno);
+        exit(0);
     }
 
     while((dataLen = read(socketfd, recvData, MAX_BUFFER)) > 0)
--- project1_5/sourceServer.cpp
+++ project1_5/sourceServer.cpp
@@ -1,14 +1,25 @@
-#include	<sys/types.h>	/* basic system data types */
-#include	<sys/socket.h>	/* basic socket definitions */
-
-#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
-#include	<arpa/inet.h>	/* inet(3) functions */
-
-#include <stdio.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <string.h>  
+#include <errno.h>  
+#include <sys/types.h>  
+#include <sys/socket.h>  
+#include <netinet/in.h> 
+#include <time.h>
 #include <unistd.h>
+
+
+//#include	<sys/types.h>	/* basic system data types */
+//#include	<sys/socket.h>	/* basic socket definitions */
+
+//#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
+//#include	<arpa/inet.h>	/* inet(3) functions */
+
+//#include <stdio.h>
+//#include <string.h>
+//#include <sys/stat.h>
+//#include <fcntl.h>
+//#include <unistd.h>
 #include <time.h>
 
 #define MAX_BUFFER (1024)
@@ -27,7 +38,7 @@ int main(int argc, char **argv)
 
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(13);
+    servaddr.sin_port = htons(8000);
 
     bind(socketfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
 
